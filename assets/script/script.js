b38dc00bf18a9b40a7ab4cdd985ad2dba1f10467
geraldosbn <geraldosbn92@gmail.com>
2023-07-05 13:51:53 -0300

    alteraçao valor km

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,37 +1,37 @@
-    const form = document.querySelector('#form')
+const form = document.querySelector('#form')
 
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+  const espacoCarga = event.target.querySelector('#espacoCarga').value
+  const kmPercorrido = event.target.querySelector('#kmPercorrido').value
 
+  if (espacoCarga == 'Exclusiva') {
+    const valorKm = 8.4
+    const valorFinal = kmPercorrido * valorKm
+    const valorReal = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(valorFinal)
+    const msg = `O valor do seu frete é de: ${valorReal}`
+    const resultado = document.querySelector('.resultado')
+    const msgContato = `Olá fiz um orçamento no seu site, de uma carga ${espacoCarga} com a distância de ${kmPercorrido} KM e ficou em ${valorReal} gostaria de fechar!`
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault()
-            const espacoCarga = event.target.querySelector('#espacoCarga').value
-            const kmPercorrido = event.target.querySelector('#kmPercorrido').value
-
-            if (espacoCarga == "Exclusiva") {
-                const valorKm = 6.5
-                const valorFinal = kmPercorrido * valorKm
-                const valorReal = new Intl.NumberFormat('pt-BR', {style:'currency', currency:'BRL'}).format(valorFinal)
-                const msg = `O valor do seu frete é de: ${valorReal}`
-                const resultado = document.querySelector('.resultado')
-                const msgContato = `Olá fiz um orçamento no seu site, de uma carga ${espacoCarga} com a distância de ${kmPercorrido} KM e ficou em ${valorReal} gostaria de fechar!`
-
-                resultado.innerHTML = `<p class="paragrafoResultado">${msg} <a id="contatoResultado"href="https://api.whatsapp.com/send?phone=5527998237731&text=${msgContato}">Clique aqui <img
+    resultado.innerHTML = `<p class="paragrafoResultado">${msg} <a id="contatoResultado"href="https://api.whatsapp.com/send?phone=5527998237731&text=${msgContato}">Clique aqui <img
                 src="./assets/img/whatsapp.png" id="img">entre em contato conosco!</a></p>`
+  } else if (espacoCarga == 'Complemento de Carga') {
+    const valorKm = 6.0
+    const valorFinal = kmPercorrido * valorKm
+    const valorReal = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(valorFinal)
+    const msg = `O valor do seu frete é de: ${valorReal}`
+    const resultado = document.querySelector('.resultado')
+    const msgContato = `Olá fiz um orçamento no seu site, de uma carga ${espacoCarga} com a distância de ${kmPercorrido} KM e ficou em ${valorReal} gostaria de fechar!`
 
-            } else if (espacoCarga == "Complemento de Carga") {
-                const valorKm = 4.70
-                const valorFinal = kmPercorrido * valorKm
-                const valorReal = new Intl.NumberFormat('pt-BR', {style:'currency', currency:'BRL'}).format(valorFinal)
-                const msg = `O valor do seu frete é de: ${valorReal}`
-                const resultado = document.querySelector('.resultado')
-                const msgContato = `Olá fiz um orçamento no seu site, de uma carga ${espacoCarga} com a distância de ${kmPercorrido} KM e ficou em ${valorReal} gostaria de fechar!`
-
-                resultado.innerHTML = `<p class="paragrafoResultado">${msg} <a id="contatoResultado"href="https://api.whatsapp.com/send?phone=5527998237731&text=${msgContato}">Clique aqui <img
+    resultado.innerHTML = `<p class="paragrafoResultado">${msg} <a id="contatoResultado"href="https://api.whatsapp.com/send?phone=5527998237731&text=${msgContato}">Clique aqui <img
                 src="./assets/img/whatsapp.png" id="img">entre em contato conosco!</a></p>`
-            }
-            else {
-                alert('Selecione o Espaço necessario!')
-            }
-
-
-        })
+  } else {
+    alert('Selecione o Espaço necessario!')
+  }
+})
